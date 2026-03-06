ea30b32a5b014693e4fedb9b1f7e3eddc2768996
RachelPezzin <rachelpezzin@gmail.com>
2026-03-06 15:56:27 -0300

    separeçao da atividade 1 em uma função

--- aula2_gpio/main.c
+++ aula2_gpio/main.c
@@ -9,17 +9,17 @@
 #include <xc.h>
 #include "util/delay.h"
 
-/*o que esse código faz?
+/*@brief
 piscar um led ligado ao pino PC2 enquanto a chave ligada ao pino PD3 estiver fechado*/
 
 // #define BIT3_MASK 0b00001000 //byte que armazena o valor da entrada da chave PD3 
 
-int main(void) {
-
+void atividade1(){
+	
 	DDRC |= (1<<DDC2);	//cofigurar o pino PC2 como saida p-72 datasheet
 	DDRD &=(1<<DDD3);   //configura o pino PD3 como entrada
     while(1) {
-       if((PIND & (1<<PIND3)) !=0) {//ler o valor da entrada da chave (1 ou 0) PINO d3(PD3) (0 ou 8 por conta de estar no 4° byte(2^4)-(0b00001000) -- & = e
+       if((PIND & (1<<PIND3)) !=0) {//ler o valor da entrada da chave (1 ou 0) PINO d3(PD3) (0 ou 8 por conta de estar no 4ï¿½ byte(2^4)-(0b00001000) -- & = e
 	   //chave pressionada?
 	   //se chave aberta = nivel logico baixo se fechada = nivel ogico alto
 	   PORTC |= (1<<PORTC2);//acende led
@@ -30,4 +30,7 @@ int main(void) {
 			PORTC &= ~(1<<PORTC2);//apaga led
 		}
     }
+}
+int main(void) {
+	atividade();
 }
