dcfba6a2bd440d5083afae62f42c60cd3e111a79
RachelPezzin <rachelpezzin@gmail.com>
2026-03-13 15:12:47 -0300

    1 mudanças da aula 3

--- aula3_interrupts/main.c
+++ aula3_interrupts/main.c
@@ -10,17 +10,28 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+int gCount = 0;
+
 ISR(INT0_vect){
 	PORTC ^= (1 << PORTC0); 
 	_delay_ms(100);
+	PORTC = 0;
+}
+
+ISR(INT1_vect){
+	gCount++;
+	PORTC ^= (1 << PORTC0);
+	_delay_ms(100);
+	PORTC = 0;
+	gCount = 0;
 }
 
 int main(void){
 
 	
 	DDRC |= (1 << DDC0);
-	EICRA = (1 << ISC01) | (1 << ISC00);
-	EIMSK = (1 << INT0);
+	EICRA = (0 << ISC01) | (1 << ISC00) | (1<<ISC11) | (1<<ISC11);
+	EIMSK = (1 << INT1)|(1<<INT0);
 	sei();
 	
 	while(1)
