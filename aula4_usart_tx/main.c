590d7598ae5e4b3230063a5d3fbf7cf2c439beb4
RachelPezzin <rachelpezzin@gmail.com>
2026-03-27 14:34:39 -0300

    20260327

---
+++ aula4_usart_tx/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 3/27/2026 1:57:54 PM
+ *  Author: rachel
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
