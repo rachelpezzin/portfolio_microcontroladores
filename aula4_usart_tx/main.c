12de27e0867c181cb7ca09b03302e7ffb1eda56d
RachelPezzin <rachelpezzin@gmail.com>
2026-03-27 15:52:40 -0300

    20260327.2

--- aula4_usart_tx/main.c
+++ aula4_usart_tx/main.c
@@ -5,12 +5,40 @@
  *  Author: rachel
  */ 
 
-#include <xc.h>
+#define F_CPU 16000000 // define frequencia do processador
 
-int main(void)
-{
-    while(1)
-    {
-        //TODO:: Please write your application code 
+#include <xc.h>
+#include "util/delay.h"
+
+uint8_t gMessage[3] = {200, 100, 50};
+	
+void USART_sendMessage (uint8_t * pData, int pSize){
+	uint8_t *tMsgPtr = pData;
+	uint8_t tChecksum = 0;
+	for(int i=0; i<pSize; i++){
+		while ((UCSR0A &(1<<UDRE0)) == 0);
+		uint8_t tMessageByte = *tMsgPtr;
+		tChecksum += tMessageByte;
+		UDR0 = tMessageByte;
+		UDR0 = *tMsgPtr++;
+	}
+	while ((UCSR0A &(1<<UDRE0)) == 0);
+	UDR0 = tChecksum;
+}
+
+
+int main(void){
+	
+	UBRR0 = 103;
+	UCSR0A = (0<<U2X0);
+	UCSR0B = (1<<TXEN0)|(0<<UCSZ02);
+	UCSR0C = (0<<UMSEL00)| (0<<UMSEL01)
+			| (1<<UPM01)| (0<<UPM00)
+			| (0<<USBS0)
+			| (1<<UCSZ01) |(1<<UCSZ00);
+    while(1) {
+		USART_sendMessage (gMessage, 3);
+		_delay_ms (10);
+     
     }
 }
