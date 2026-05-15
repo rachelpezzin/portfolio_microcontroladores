78fd1bc0f3bee3fa98a83a04f051a1b4dcda61d8
RachelPezzin <rachelpezzin@gmail.com>
2026-05-15 16:04:58 -0300

    aula 7 timer - 2

--- aula7_timer/main.c
+++ aula7_timer/main.c
@@ -9,17 +9,24 @@
 #include <xc.h>
 #include "avr/interrupt.h"
 
-ISR(TIMER0_OVF_vect){
-	PORTB ^= (1<<PORTB0); //alterna PB0
+ISR(TIMER0_COMPA_vect){
+	PORTB |= (1<<PORTB0); //alterna PB0
+}
+
+ISR(TIMER0_COMPB_vect){
+	PORTB &= ~(1<<PORTB0); //alterna PB0
 }
 
 int main(void){
 	DDRB = (1<<DDB0);
+	
 	TCCR0A = (0<<WGM00)|(1<<WGM01); //MODO CTC
 	TCCR0B = (0<<WGM02)|(0<<CS02)|(1<<CS01)|(0<<CS00); // prescaler de 8
-	OCR0A = 249; //contagem ate 250us (considerando clk do timer = 1MHz
 	
-	TIMSK0 = (1<<TOIE0); //habilita interrupção de overflow  do timer 0
+	OCR0A = 249; //contagem ate 250us (considerando clk do timer = 1MHz
+	OCR0B = 99;
+	
+	TIMSK0 = (1<<OCIE0A)|(1<<OCIE0B); //habilita interrupção de overflow  do timer 0
 	sei();
     while(1){
         
